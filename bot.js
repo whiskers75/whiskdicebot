fc82de5fda2fec6891d49f6ee38871b073037107
whiskers75 <whiskers75@me.com>
2013-05-20 18:15:27 +0100

    5% rake + fee

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var random = require("secure_random");
 var users = [];
 var chatBuffer = [];
 var chance = 60;
-var edge = 0.95; // EV
+var edge = 0.93; // EV + 2% tip fee
 var payout = 1.4;
 var shutdown = false;
 var lastWinner = null;
@@ -111,7 +111,7 @@ socket.on("connect", function() {
 		
             }
             if (data.message === "!topic" && data.room === "botgames" && (data.user === "whiskers75" || data.user === "admin")) {
-                chat('botgames', '/topic The official SatoshiDice for CoinChat! | Admin approved | ' + ((1 - edge) * 100).toFixed(2) + '% house edge! | YOU decide your chances of winning! | !help for info', "000");
+                chat('botgames', '/topic The official SatoshiDice for CoinChat! | Admin approved | ' + (((1 - edge) * 100) - 2).toFixed(2) + '% house edge! | YOU decide your chances of winning! | !help for info', "000");
             }
             if (data.message === "!shutdown" && data.room === "botgames" && (data.user === "whiskers75" || data.user === "admin")) {
                 chat('botgames', '/bold Shutting down bot, no more bets please!', "505");
