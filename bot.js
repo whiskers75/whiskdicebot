1ab10a278bfc7a2a48c07c30cde9db042db9eb1b
whiskers75 <whiskers75@me.com>
2013-06-30 07:45:57 +0100

    New bet reporter - neater!

--- bot.js
+++ bot.js
@@ -150,7 +150,7 @@ socket.on("connect", function() {
 				}
 				else {
 				    db.set('winnings/' + data.user, Number(res) + Number(data.won), redis.print)
-                                    chat('botgames', '✔ ' + data.user + ': ' + Number(res) + Number(data.won) + ' mBTC! (+ ' + data.won +  ' mBTC) [' + data.chance + '% ' + data.payout + 'x: ' + data.rand + " < " + (data.chance + 1) + ']', "090");
+                                    chat('botgames', '✔ ' + data.user + ' has ' + (Number(res) + Number(data.won)).toFixed(2) + ' mBTC! (+' + data.won +  ' mBTC) [' + data.chance + '%, ' + data.payout + 'x: ' + data.rand + " < " + (data.chance + 1) + ']', "090");
 				}
 			    });
                             //chat('botgames', '!; win ' + data.user + ' ' + data.won, "000");
@@ -166,7 +166,7 @@ socket.on("connect", function() {
                                 else {
                                     data.lost = Number(data.message.substring(58, data.message.indexOf('mBTC')));
 				    db.set('winnings/' + data.user, Number(res) - data.lost, redis.print);
-				    chat('botgames', '✗ ' + data.user + ': ' + Number(res) + Number(data.lost) + ' mBTC! (- ' + data.lost + ' mBTC) [' + data.chance + '% ' + data.payout + 'x: ' + data.rand + " < " + (data.chance + 1) + ']', "e00");
+				    chat('botgames', '✗ ' + data.user + ' has ' + Number(res) + Number(data.lost) + ' mBTC! (-' + data.lost + ' mBTC) [' + data.chance + '%, ' + data.payout + 'x: ' + data.rand + " < " + (data.chance + 1) + ']', "e00");
 				}
                             });
 			    //chat('botgames', '!; loss ' + data.user + ' ' + data.message.substring(58, data.message.indexOf('mBTC') - 1), "000");
